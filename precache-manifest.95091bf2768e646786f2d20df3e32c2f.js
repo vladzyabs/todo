5af163d46833a044e69c497ad6e0cf429ed4574a
vladzyabs <vladzyaba@gmail.com>
2020-08-03 11:46:15 +0300

    Updates

---
+++ precache-manifest.95091bf2768e646786f2d20df3e32c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef7fa7627e3752ec9980ac4bed0bb6e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "55902d363a5e362d85c3",
+    "url": "/todo/static/css/main.f7c0a422.chunk.css"
+  },
+  {
+    "revision": "f4a275f8b06ffc53b3d7",
+    "url": "/todo/static/js/2.d37e2ae4.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/todo/static/js/2.d37e2ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55902d363a5e362d85c3",
+    "url": "/todo/static/js/main.ae266f5f.chunk.js"
+  },
+  {
+    "revision": "1dc100ee03e886eceda5",
+    "url": "/todo/static/js/runtime-main.c1064553.js"
+  }
+]);
